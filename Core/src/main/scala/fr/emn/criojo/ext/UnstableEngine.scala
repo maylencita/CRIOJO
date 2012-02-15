829cd1a5977d8dd4c15ca7b469f0768fb7f54737
Badock <jonathancmoa@gmail.com>
2012-02-15 15:55:35 +0100

    Core changes: - Scaladoc added to UnstableEngin - No more UnstableObject class - UnstableRelation and UnstableAtom moved into UnstableEngine - Some javadoc syntax error corrected in JSONUtil methods

--- Core/src/main/scala/fr/emn/criojo/ext/UnstableEngine.scala
+++ Core/src/main/scala/fr/emn/criojo/ext/UnstableEngine.scala
@@ -10,21 +10,35 @@ import fr.emn.criojo.core._
  * To change this template use File | Settings | File Templates.
  */
 
-import fr.emn.criojo.core.Criojo._
-import collection.mutable.Buffer
 import fr.emn.criojo.lang.ChamGuard
+import fr.emn.criojo.lang.{CrjAtom, Molecule}
+import collection.mutable.{Buffer}
+import fr.emn.criojo.core._
 
+
+/**
+ * The UnstableEngine class, an engine that can handle rules with temporary variables in the left body
+ * @define THIS UnstableEngine
+ * @define PARENT StatefulEngine
+ */
 class UnstableEngine extends StatefulEngine {
-
-
-
 
   def Equal(term1: Term, term2: Term): Guard = new EqualsGuard(term1, term2) with ChamGuard
   def NotEqual(term1: Term, term2: Term): Guard = new NotEqualsGuard(term1, term2) with ChamGuard
   //def OneIsTrue(guards:List[Guard]):Guard = new OneIsTrueGuard(guards.toList) with ChamGuard
 
-  // this method has been override to include a specific case for UnstableAtom
+
+  /** Create a rule that can handle rules with temporary variables in the left body
+   *
+   * @param h head of the rule
+   * @param b body of the rule
+   * @param g guards of the rule
+   * @param scope list of variable
+   * @return the new rule
+   */
+
   override def createRule(h: Head, b: Body, g: Guard, scope: List[Variable]) = {
+  // this method has been override to include a specific case for UnstableAtom
 
     var headList: List[Atom] = List()
     var unstableList: List[UnstableAtom] = List()
@@ -39,11 +53,20 @@ class UnstableEngine extends StatefulEngine {
     new UnstableRule(headList, b, g, scope, unstableList)
   }
 
+  /**
+   * The UnstableRule class, a rule that can have temporary variables in her left body
+   * @define THIS UnstableRule
+   * @define PARENT StatefulRule
+   */
   class UnstableRule(head: List[Atom], body: List[Atom], guard: Guard, scope: List[Variable],
                      val unstableRelations: List[UnstableAtom]) extends StatefulRule(head, body, guard, scope) {
 
-    // since the introduction of UnstableRule, some parts of the algorithm needs specific code
+    /**
+     * Check if every variable is available. If so, apply the rule
+     * @param finalExecution the execution context
+     */
     override def applyReaction(finalExecution: PartialExecution) {
+    // since the introduction of UnstableRule, some parts of the algorithm needs specific code
       try {
         val scopeSubs = scope.map {
           v => val i = Indexator.getIndex; (v, v + ("@" + i))
@@ -102,4 +125,174 @@ class UnstableEngine extends StatefulEngine {
     }
   }
 
+
+  /**
+   * The UnstableRelation class
+   * @define THIS UnstableRelation
+   * @define PARENT LocalRelation
+   */
+  class UnstableRelation(name: String, f: (Tuple2[Product, Product]
+                        ,Buffer[(Variable, Term)]) => Unit) extends LocalRelation(name, true) {
+
+
+    /** Apply the parameters to the rules. Here the parameters values are not interpreted, the engine will only store
+     *  the variables references.
+     *
+     * @param t a couple that will contains the transformation expression (eg: (x,y) --> (z))
+     * @return an UnstableAtom molecule
+     */
+    def apply(t: Tuple2[Product, Product]): Molecule = {
+
+      var param1: List[Variable] = List()
+      var param2: List[Variable] = List()
+
+      // we identify two cases:
+      // case x: Variable means => (x) -> [...]
+      // case x: Product  means => (x,y,z,t) -> [...]
+      t._1 match {
+        case x: Variable => {
+          param1 = x :: param1
+        }
+        case x: Product => {
+          param1 = x.productIterator.toList.asInstanceOf[List[Variable]]
+        }
+        case _ => throw new Exception("Bad arguments: when creating\"" + name + "\"(a -> b), a should be a Term*")
+      }
+
+      // we identify two cases:
+      // case x: Variable means => [...] -> (x)
+      // case x: Product  means => [...] -> (x,y,z,t).
+      t._2 match {
+        case x: Variable => {
+          param2 = x :: param2
+        }
+        case x: Product => {
+          param2 = x.productIterator.toList.asInstanceOf[List[Variable]]
+        }
+        case _ => throw new Exception("Bad arguments: when creating\"" + name + "\"(a -> b), b should be a Term*")
+      }
+
+      val unstableAtom: UnstableAtom = new UnstableAtom(name, f, t, param1 ::: param2)
+      unstableAtom.setParams(param1, param2)
+      unstableAtom
+    }
+
+
+  }
+
+  /**
+   * The UnstableAtom class
+   * @define THIS UnstableAtom
+   * @define PARENT CrjAtom
+   */
+  class UnstableAtom(relName: String, val f: (Tuple2[Product, Product], Buffer[(Variable, Term)]) => Unit, exp: Tuple2[Product, Product], terms: List[Term]) extends CrjAtom(relName, terms) {
+
+    //override def toString():String = "UnstableAtom"
+
+    var inputParams: List[Term] = List()
+    var outputParams: List[Variable] = List()
+    var missingVariable: Int = -1;
+
+    /** Store the given parameters as input and output parameters
+     *  the variables references.
+     *
+     * @param param1 a list of input variable
+     * @param param2 a list of output variable
+     */
+    def setParams(param1: List[Variable], param2: List[Variable]) = {
+      inputParams = param1
+      outputParams = param2
+    }
+
+
+    /** Evaluate the rule with the given substitutions
+     *  the variables references.
+     *
+     * @param subs a list of substitutions
+     * @return an integer: 0 => no more variable available since the last "eval" call
+     *    1 => some variable are now available but "eval" need some other values
+     *    2 => anything is available and the rule has been correctly computed
+     */
+    def eval(subs: Buffer[(Variable, Term)]): Int = {
+
+      val missingVariableBeforeThisIteration = missingVariable // save the previous value of missing value, so that we will be able to detect any changes
+
+      missingVariable = 0
+
+      var inputArgsExp: List[(Variable, Value[Int])] = Nil
+
+      // take the (Variable | Term)* input expression and construct a ( (Variable | Term), ValueTerm )* expression
+      try {
+        inputParams.foreach(p => {
+          p match {
+            case x: Variable => {
+              val res = subs.find(s => s._1.name == x.name)
+              res match {
+                case Some((y, v: Value[Int])) => {
+                  inputArgsExp = inputArgsExp ::: ((x, new ValueTerm[Int](v.getValue)) :: Nil)
+                  true
+                }
+                case _ => {
+                  missingVariable = missingVariable + 1; false
+                }
+              }
+            }
+            case t: Value[Int] => {
+              inputArgsExp = inputArgsExp ::: List[(Variable, Value[Int])]((new Variable("_"), t))
+            }
+            case _ => {}
+          }
+        })
+      } catch {
+        case ex: Exception => {
+          // TODO: check if execeptions are useful for this part?
+          ex.printStackTrace()
+        }
+      }
+
+      var outputArgsExp: List[(Variable, Value[Int])] = Nil
+
+      // take the (Variable | Term)* output expression and construct a ( (Variable | Term), ValueTerm )* expression
+      outputParams.foreach(p => {
+        p match {
+          case x: Variable => {
+            outputArgsExp = outputArgsExp ::: ((x, new MutableValueTerm[Int](0)) :: Nil)
+          }
+          case t => {
+          }
+        }
+      })
+
+      var exp2: Tuple2[Product, Product] = (inputArgsExp, outputArgsExp)
+
+      // finalValue is the value returned by the function:
+      //    0 : no more variable available since the last "eval" call
+      //    1 : some variable are now available but "eval" need some other values
+      //    2 : anything is available
+
+      var finalValue: Int = -1
+      // everything is OK => 2 (all variables are available)
+      if (missingVariable == 0) {
+        // evaluating the function
+        f(exp2, subs)
+
+        // insertion of the new values for the intermediary variable
+        exp2._2.asInstanceOf[List[(Variable, ValueTerm[Int])]].foreach(s => {
+          subs.append(s)
+        })
+
+        finalValue = 2
+      }
+      else
+      // no progress => 0 (no more values)
+      if (missingVariable == missingVariableBeforeThisIteration)
+        finalValue = 0
+      else
+      // a progress => 1 (some previously unavailable values are available)
+      if (missingVariable < missingVariableBeforeThisIteration || missingVariableBeforeThisIteration == -1)
+        finalValue = 1
+
+      finalValue
+    }
+  }
 }
